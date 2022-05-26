3b5a1dc9bf61789475634120609c8ba31083463e
Your Name <you@example.com>
2022-05-26 23:19:31 +0600

    portfolio updated

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,17 +1,49 @@
 import React, { useState } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, NavLink, Row } from "react-bootstrap";
 import styles from "./Portfolio.module.css";
 const Portfolio = () => {
 
   const projectName = [
-    { imgId: "01", imgUrl: "/images/project01.png" },
-    { imgId: "02", imgUrl: "/images/project02.png" },
-    { imgId: "03", imgUrl: "/images/project03.png" },
-    { imgId: "04", imgUrl: "/images/project04.png" },
-    { imgId: "05", imgUrl: "/images/project05.png" },
-    { imgId: "06", imgUrl: "/images/project06.png" },
-    { imgId: "07", imgUrl: "/images/project07.png" },
-    { imgId: "08", imgUrl: "/images/project08.png" },
+    {
+      imgId: "01",
+      imgUrl: "/images/project01.png",
+      webLink: "https://medicare-c15c1.web.app/",
+    },
+    {
+      imgId: "02",
+      imgUrl: "/images/project02.png",
+      webLink: "https://food-sender-muhammadsabbir.web.app/",
+    },
+    {
+      imgId: "03",
+      imgUrl: "/images/project03.png",
+      webLink: "https://drone-galaxy-muhammad.web.app/",
+    },
+    {
+      imgId: "04",
+      imgUrl: "/images/project04.png",
+      webLink: "https://car-bro-4747.firebaseapp.com/",
+    },
+    {
+      imgId: "05",
+      imgUrl: "/images/project05.png",
+      webLink: "https://project-pi-b18db.web.app/login",
+    },
+    {
+      imgId: "06",
+      imgUrl: "/images/project06.png",
+      webLink: "https://incredible-tulumba-e22a3c.netlify.app/",
+    },
+    {
+      imgId: "07",
+      imgUrl: "/images/project07.png",
+      webLink: "https://sabbir1054.github.io/Practice-Project-19/",
+    },
+    {
+      imgId: "08",
+      imgUrl: "/images/project08.png",
+      webLink: "https://sabbir1054.github.io/caravana-pizza-restaurant/",
+    },
   ];
   return (
     <div className="position-relative" id="portfolio">
@@ -34,7 +66,7 @@ const Portfolio = () => {
                 />
                 <div className={`${styles.overlay} text-center`}>
                   <div className={`${styles.text} btn btn-primary`}>
-                    Click Here
+                    <NavLink href={`${data.webLink}`} target='blank' className='text-white'>Click Here</NavLink>
                   </div>
                 </div>
               </Card>

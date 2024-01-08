867ea4378fbf4a901f0a828587bba6c254392757
sabbir1054 <mdsabbir1054@gmail.com>
2024-01-08 20:22:08 +0600

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
 const Footer = () => {
-    return (
-        <div className='position-relative pt-5 mt-5'>
-            <div className={`${styles.footer_wrapper} text-center`}>
-                <div className="footer-img d-flex justify-content-center">
-                    <img src="/images/logo.png" alt="" width={100}/>
-                </div>
-                <h3 className='text-white text-center'>Infinity Algo Station</h3>
-                <p className="fs-5 text-white">
-                    Copyright @ 2022-2024 infinityalgostation.com . All Rights Reserved | <span className='fw-bold'>Privacy Policy</span>
-                </p>
-            </div>
+  return (
+    <div className="position-relative pt-5 mt-5">
+      <div className={`${styles.footer_wrapper} text-center`}>
+        <div className="footer-img d-flex justify-content-center">
+          <img src="/images/logo.png" alt="" width={100} />
         </div>
-    );
+        <h3 className="text-white text-center">Infinity Algo Station</h3>
+        <h5 className="text-white text-center">
+          <span className="px-2">email: infinityalgostation@gmail.com </span>
+          <span className="px-2">Mobile: 01322501697 </span>
+        </h5>
+        <p className="fs-5 text-white">
+          Copyright @ 2022-2024 infinityalgostation.com . All Rights Reserved |{" "}
+          <span className="fw-bold">Privacy Policy</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -4,69 +4,48 @@ import Slider from "react-slick";
 import SingleTeam from "./SingleTeam";
 import styles from "./Team.module.css";
 const Team = () => {
-    const member = [
-      {
-        memberId: "01",
-        memberName: "Atiqul Ansari",
-        memberPhoto: "/images/photo1.jpeg",
-        memberPosition: "Principal & Creative Director (CEO)",
-        fid: "https://facebook.com/atiqul.ansari.75",
-        wts: "01717865265",
-        git: "https://github.com/atiqul123",
-        linked: "",
-      },
-      {
-        memberId: "00",
-        memberName: "Md. Masum Billah",
-        memberPhoto: "/images/11.JPG",
-        memberPosition: "Project Advisor  and Lecturer Department of (ITM) DIU",
-        fid: "https://facebook.com/mdmasumbillahswe",
-        wts: "01708611106",
-        git: "",
-        linked: "https://www.linkedin.com/in/md-masum-billah-78a185175/",
-      },
-
-      {
-        memberId: "02",
-        memberName: "Muhammad Sabbir",
-        memberPhoto: "/images/photo2.png",
-        memberPosition: "Senior MERN  Stack Developer (Project Manager)",
-        fid: "https://facebook.com/sabbir.1054/",
-        wts: "01733208221",
-        git: "https://github.com/sabbir1054",
-        linked: "https://www.linkedin.com/in/md-sabbir-hossain-1054/",
-      },
-      {
-        memberId: "03",
-        memberName: "Ebrahim Rhaman",
-        memberPhoto: "/images/photo3.png",
-        memberPosition: "Senior Mobile App (Android) Developer",
-        fid: "https://facebook.com/profile.php?id=100087952092677",
-        wts: "01749989506",
-        git: "https://github.com/Ebrahim683",
-        linked: "https://www.linkedin.com/in/md-ebrahim-rahman-345620185/",
-      },
-      {
-        memberId: "04",
-        memberName: "Nur Hossain",
-        memberPhoto: "/images/pcf.png",
-        memberPosition: "Senior Frontend Developer (MERN)",
-        fid: "https://facebook.com/nurhossainfarid221",
-        wts: "01841268946",
-        git: "https://github.com/nurhossainfarid",
-        linked: "https://www.linkedin.com/in/nur-hossain-farid-58725522a/",
-      },
-      {
-        memberId: "05",
-        memberName: "Tareque Ahmed",
-        memberPhoto: "/images/photo5.png",
-        memberPosition: "Senior Web Designer and Developer",
-        fid: "https://facebook.com/mdtareque.ahmed.77",
-        wts: "01712418146",
-        git: "https://github.com/TAREQUE-AHMED",
-        linked: "https://www.linkedin.com/in/mdtarequeahmed146/",
-      },
-    ];
+  const member = [
+    {
+      memberId: "01",
+      memberName: "Atiqul Ansari",
+      memberPhoto: "/images/photo1.jpeg",
+      memberPosition: "Founder & Creative Director",
+      fid: "https://facebook.com/atiqul.ansari.75",
+      wts: "01717865265",
+      git: "https://github.com/atiqul123",
+      linked: "",
+    },
+    {
+      memberId: "02",
+      memberName: "Muhammad Sabbir",
+      memberPhoto: "/images/photo2.png",
+      memberPosition: "CEO & Full Stack Developer",
+      fid: "https://facebook.com/sabbir.1054/",
+      wts: "01733208221",
+      git: "https://github.com/sabbir1054",
+      linked: "https://www.linkedin.com/in/md-sabbir-hossain-1054/",
+    },
+    {
+      memberId: "03",
+      memberName: "Ebrahim Rhaman",
+      memberPhoto: "/images/photo3.png",
+      memberPosition: "Project Manager &  Mobile App Developer",
+      fid: "https://facebook.com/profile.php?id=100087952092677",
+      wts: "01749989506",
+      git: "https://github.com/Ebrahim683",
+      linked: "https://www.linkedin.com/in/md-ebrahim-rahman-345620185/",
+    },
+    {
+      memberId: "05",
+      memberName: "Tareque Ahmed",
+      memberPhoto: "/images/photo5.png",
+      memberPosition: "Senior Web Designer and Developer",
+      fid: "https://facebook.com/mdtareque.ahmed.77",
+      wts: "01712418146",
+      git: "https://github.com/TAREQUE-AHMED",
+      linked: "https://www.linkedin.com/in/mdtarequeahmed146/",
+    },
+  ];
   const settings = {
     dots: true,
     infinite: true,
@@ -104,20 +83,21 @@ const Team = () => {
     ],
   };
   return (
-    <div className={`${styles.team_wrapper} position-relative py-5 bg-light`} id='team'>
+    <div
+      className={`${styles.team_wrapper} position-relative py-5 bg-light`}
+      id="team"
+    >
       <Container className="py-5">
         <div className={`${styles.team_text}`}>
           <h2 className="text-center fw-bold pb-4">
             Our <span className={styles.colored_text}>Team</span>
           </h2>
-        </div>
-          {" "}
-          <Slider {...settings}>
-            {member.map((data) => (
-              <SingleTeam data={data} key={data.memberId}></SingleTeam>
-            ))}
-          </Slider>
-       
+        </div>{" "}
+        <Slider {...settings}>
+          {member.map((data) => (
+            <SingleTeam data={data} key={data.memberId}></SingleTeam>
+          ))}
+        </Slider>
       </Container>
     </div>
   );
